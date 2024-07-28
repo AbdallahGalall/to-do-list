d649739c028c4393db67557eff411512dab9ef61
abdallah galal <abdallahgalal2003@gmail.com>
2024-07-28 20:58:25 +0300

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace to_do_list.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static List<items> item = new List<items>();
+        private static int id = 1;
+        [HttpGet("getbyid")]
+        public IActionResult getitembyid(int id)
+        {
+            var e = item.SingleOrDefault(x => x.Id == id);
+            if (e == null)
+            {
+                return NotFound();
+            }
+            return Ok(e);
+        }
+        [HttpPost("creatnewitem")]
+        public IActionResult creattitem(items new_item)
+        {
+            new_item.Id = id++; ;
+            item.Add(new_item);
+            return CreatedAtAction(nameof(getitembyid), new { iid = new_item.Id }, new_item);
+        }
+
+
+        [HttpPatch("{status}")]
+        public IActionResult statuspdate(bool status, items iitem)
+        {
+
+            var e = item.SingleOrDefault(x => x.Id == iitem.Id);
+            if (e == null) { return NotFound(); }
+            e.status = status;
+            return Ok(e);
+        }
+        [HttpPut]
+        public IActionResult updateitems(items itemsss)
+
+        {
+            var e = item.SingleOrDefault(x => x.Id == itemsss.Id);
+            if (e == null) { return NotFound(); }
+            e.Task_desc = itemsss.Task_desc;
+            e.priority = itemsss.priority;
+            e.status = itemsss.status;
+            return Ok(e);
+        }
+    
+    [HttpDelete]
+    public IActionResult deleteitem(int id) 
+    {
+        var e = item.SingleOrDefault(items => items.Id == id);
+            if (e == null) { return NotFound(); };
+            //if the item not done iw won't be deleted 
+            if(e.status==false) {  return BadRequest(); }
+            item.Remove(e);
+            return NoContent();
+    }
+    }
+
+}
---
+++ items.cs
@@ -0,0 +1,11 @@
+﻿namespace to_do_list
+{
+    public class items
+    {
+        public int Id { get; set; }
+        public string Task_desc { get; set; }
+        public string priority { get; set; }
+        public bool status {  get; set ; }=false;
+      
+    }
+}
